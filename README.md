5ae5a4c5dd3f50006758d5107677c8b5c8736aee
Noah Santschi-Cooney <noah@santschi-cooney.ch>
2019-09-21 19:45:09 +0100

    Added local testing/development v0.69.ru

--- README.md
+++ README.md
@@ -1,6 +1,26 @@
 Netsoc Ansible
 =========
 
+## Local Development
+
+### Prerequisites
+
+1. Hashicorp Packer
+2. LXD up and running
+
+### How 2
+
+1. Run `./simulation.sh` and wait for everything to startup.
+2. Change the IPs in `./host_vars/*` to the ones output in the console
+
+The IPs only need to be updated once or after every time that you run `./simulation.sh teardown`
+
+SSH Keys are generated and placed into the correct directories. You can SSH to each container/host using username `netsoc` and password `borger`
+
+Make sure not to commit the host_vars with changed IPs.
+
+## Misc Info
+
 ssh priv key structure:
 ```/keys/<host>/id_rsa```
 
---
+++ simulation.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# This spins up a simulation of the Netsoc infrastructure (just the 4 main servers so far) using (currently) LXD
+
+SERVERS=(leela bigbertha boole lovelace)
+
+if [[ $1 == 'teardown' ]]; then
+    for server in ${SERVERS[@]}; do
+        printf '.'
+        lxc stop $server
+        printf '.'
+        lxc rm $server
+    done
+    echo 'Done!'
+    exit 0
+fi
+
+
+if [ $(lxc image list --format json | jq -r '.[] | select(.aliases[0].name == "netsoc_infra").aliases[0].name') != 'netsoc_infra' ]; then
+    echo 'LXD image aliased "netsoc_infra" not found. Please build the LXD image using Packer and the packer.json file in ./packer'
+    exit 1
+fi
+
+for server in ${SERVERS[@]}; do
+    lxc launch netsoc_infra $server -c security.nesting=true
+done
+
+echo 'Sleeping to give containers time to obtain ipv4 address...'
+sleep 5
+
+IPS=()
+
+echo ''
+echo "Change the IP in ./host_vars to the following IPs. Don't forget to not commit those changes!"
+for server in ${SERVERS[@]}; do
+    ip=$(lxc list --format json $server | jq -r .[0].state.network.eth0.addresses[0].address)
+    echo "$server: $ip"
+    ssh-keyscan -H $ip >> ~/.ssh/known_hosts > /dev/null
+    IPS+=($ip)
+done
+
+for i in "${!SERVERS[@]}"; do 
+    server=${SERVERS[$i]}
+    ip=${IPS[$i]}
+
+    echo -e 'y\n' | ssh-keygen -b 2048 -t rsa -N '' -f ./keys/$server/id_rsa > /dev/null
+    echo borger | sshpass ssh-copy-id -i ./keys/$server/id_rsa netsoc@$ip > /dev/null
+done
