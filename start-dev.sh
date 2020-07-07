ccf2218165d8517806cf6d6615e398c9d0b6239a
ocanty <git@ocanty.com>
2020-07-07 20:24:06 +0100

    start-dev now installs deps

--- start-dev.sh
+++ start-dev.sh
@@ -2,7 +2,8 @@
 
 if [ ! -d "./bin" ]; then
 	virtualenv -p /usr/bin/python2.7 .
-	#pip install requirements.txt
+	source bin/activate
+	pip install requirements.txt
 fi
 
 bash --init-file <(echo "source bin/activate; source proxmox_secrets.sh")
