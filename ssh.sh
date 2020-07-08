3145a11919e9db3c60ddbd1666c8c25689be59ec
ocanty <git@ocanty.com>
2020-07-08 02:06:37 +0100

    moved ipa.netsoc.co to ipa.netsoc.dev

--- ssh.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-hostname="$1.vm.netsoc.co"
-ip=`./proxmox_inventory.py --list | jq '._meta.hostvars["'$"$hostname"'"].ansible_host' | sed 's/"//g'`
-key=`./proxmox_inventory.py --list | jq '._meta.hostvars["'$"$hostname"'"].ansible_ssh_private_key_file' | sed 's/"//g'`
-echo $ip
-shift
-ssh netsoc@$ip -i $key -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=ERROR $@
