05e66740ab0b7d1a41846855b7f9e45f8c93022c
Eric Moynihan <git@moynihan.io>
2020-10-16 13:01:41 +0100

    Added optional zsh usage for start-dev

--- start-dev.sh
+++ start-dev.sh
@@ -7,4 +7,11 @@ if [ ! -d "./bin" ]; then
 	ansible-galaxy install -r requirements.yml
 fi
 
+if [[ $# -eq 1 ]] ; then
+    if [[ $1 == 'zsh' ]] ; then
+		zsh -c 'source bin/activate; source proxmox_secrets.sh; zsh -i'
+    fi
+    exit 0
+fi
+
 bash --init-file <(echo "source bin/activate; source proxmox_secrets.sh")
