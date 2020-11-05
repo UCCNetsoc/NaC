b6da44ca6dfe7c611767dc038e652f5c835aac1b
Eric Moynihan <git@moynihan.io>
2020-11-05 16:09:49 +0000

    Automated retrieval of vault password

--- vault-edit.sh
+++ vault-edit.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 printf "Run export EDITOR=<your choice of editor> to change the editor in use\n"
 printf "If you get permission denied when running this script, you don't have an editor set\n"
-ansible-vault edit vars/secrets.yml
+if [[ ! -z "${VAULT_PASS}" ]]; then
+    echo "$VAULT_PASS" > ./_vault_pass
+    ansible-vault edit vars/secrets.yml --vault-password-file ./_vault_pass
+    rm _vault_pass
+else
+    ansible-vault edit vars/secrets.yml
+fi
