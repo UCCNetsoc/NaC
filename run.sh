e16aea199f7a42c17a97283493045541d68a958b
Eric Moynihan <git@moynihan.io>
2020-11-05 16:22:00 +0000

    No vault pass required for playbooks

--- run.sh
+++ run.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-ansible-playbook -i proxmox_inventory.py -i hosts --ask-vault-pass ${@:1}
+if [[ ! -z "${VAULT_PASS}" ]]; then
+    echo "$VAULT_PASS" > ./_vault_pass
+    ansible-playbook -i proxmox_inventory.py -i hosts --vault-password-file ./_vault_pass ${@:1} || true
+    rm _vault_pass
+else
+    ansible-playbook -i proxmox_inventory.py -i hosts --ask-vault-pass ${@:1}
+fi
