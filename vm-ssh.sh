b0334ab8b49e10d9aff32fc1707e62bb1bcd573f
Eric Moynihan <git@moynihan.io>
2021-09-04 15:28:10 +0100

    Added interactive vm-ssh.sh options

--- vm-ssh.sh
+++ vm-ssh.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 hostname="$1"
+if [[ $hostname == "" ]] ; then
+	arr=( $(./proxmox_inventory.py --list | jq -r '._meta.hostvars | keys | .[]') )
+	select opt in "${arr[@]}"; do
+		export hostname="$opt"
+		break
+	done
+fi
 ip=`./proxmox_inventory.py --list | jq '._meta.hostvars["'$"$hostname"'"].ansible_host' | sed 's/"//g'`
 key=`./proxmox_inventory.py --list | jq '._meta.hostvars["'$"$hostname"'"].ansible_ssh_private_key_file' | sed 's/"//g'`
 echo "Connectng to $ip using netsoc with $key"
