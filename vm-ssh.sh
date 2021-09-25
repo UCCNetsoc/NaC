9da28748089909718b84171dcae59600201e6eda
Eric Moynihan <git@moynihan.io>
2021-09-25 15:49:07 +0100

    vm-ssh works with infra containers

--- vm-ssh.sh
+++ vm-ssh.sh
@@ -10,6 +10,7 @@ if [[ $hostname == "" ]] ; then
 fi
 ip=`./proxmox_inventory.py --list | jq '._meta.hostvars["'$"$hostname"'"].ansible_host' | sed 's/"//g'`
 key=`./proxmox_inventory.py --list | jq '._meta.hostvars["'$"$hostname"'"].ansible_ssh_private_key_file' | sed 's/"//g'`
+user=`./proxmox_inventory.py --list | jq '._meta.hostvars["'$"$hostname"'"].user' | sed 's/"//g'`
 echo "Connectng to $ip using netsoc with $key"
 shift
-ssh netsoc@$ip -i $key -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=ERROR $@
+ssh $user@$ip -i $key -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=ERROR $@
